0042be88239bb752d92feba043c5a85edadf7aea
Elise-r01 <elise01rou@gmail.com>
2025-02-03 16:37:37 +0100

    amélioration css et js avec aide Virginie

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById('author-btn');
 btn.addEventListener('click', () => {
     const author = document.getElementById('author-name');
+    author.classList.remove('off');
     author.classList.add('on');
     btn.classList.add('off');
 });
